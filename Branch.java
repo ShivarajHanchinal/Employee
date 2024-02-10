bb37e3dd595414136bbbd1529ac30437cf24224f
ShivarajHanchinal <01fe22bca049@kletech.ac.in>
2024-02-10 11:20:25 +0530

    Extended branch

---
+++ Branch.java
@@ -0,0 +1,13 @@
+public class Branch {
+    public static void  department(int id)
+    {
+        if(id==101)
+        {
+            System.out.println("CS");     
+        }
+        else if(id==102)
+        {
+            System.out.println("Civil");
+        }
+    }    
+}
